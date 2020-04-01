516479f0f2b00ccc30217b937e19160c596052c2
thiago william <thiago.will.silva@gmail.com>
2020-04-01 13:49:44 +0100

    Updates

---
+++ precache-manifest.b5ad2a924471e33a545e4df3e1dd7368.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0737ab62bd3b3d8e549640390786b090",
+    "url": "/new-portfolio/index.html"
+  },
+  {
+    "revision": "6c0d2209eedc405c246c",
+    "url": "/new-portfolio/static/js/2.82f780a2.chunk.js"
+  },
+  {
+    "revision": "115c29c453b61995b5be0af8c45bd83e",
+    "url": "/new-portfolio/static/js/2.82f780a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5495886d44cc6e6ef6a6",
+    "url": "/new-portfolio/static/js/main.b23ec15d.chunk.js"
+  },
+  {
+    "revision": "0a0c1a941c8d10b2dd16",
+    "url": "/new-portfolio/static/js/runtime-main.32b3caa7.js"
+  },
+  {
+    "revision": "44ca80d058015eaac1e25e1944e3cb18",
+    "url": "/new-portfolio/static/media/Thiago-William-CV-English.44ca80d0.pdf"
+  },
+  {
+    "revision": "a584ff3bb9715a79971ff3e59dc71ccd",
+    "url": "/new-portfolio/static/media/bg-contato.a584ff3b.svg"
+  },
+  {
+    "revision": "80f74f6b6a5b664d5d8a70f2d896e1b7",
+    "url": "/new-portfolio/static/media/bg-home.80f74f6b.png"
+  },
+  {
+    "revision": "14306565616ef357769f75028d0374a9",
+    "url": "/new-portfolio/static/media/icon-css3.14306565.svg"
+  },
+  {
+    "revision": "21c684b0ea517ed928e4a483fe9cbc1b",
+    "url": "/new-portfolio/static/media/icon-gatsby.21c684b0.svg"
+  },
+  {
+    "revision": "55b7a21a3dbc09549bbf527255c4cf07",
+    "url": "/new-portfolio/static/media/icon-git.55b7a21a.svg"
+  },
+  {
+    "revision": "9a4c256a6e5b61056f5ba5505a7971f5",
+    "url": "/new-portfolio/static/media/icon-html5.9a4c256a.svg"
+  },
+  {
+    "revision": "36da9fa85e8d7e65788d0674c59ff318",
+    "url": "/new-portfolio/static/media/icon-javascript.36da9fa8.svg"
+  },
+  {
+    "revision": "0e7a4fe0b3e8d07fe4b0ee33281e8614",
+    "url": "/new-portfolio/static/media/icon-reactjs.0e7a4fe0.svg"
+  },
+  {
+    "revision": "2a146488374679891255e3248849a806",
+    "url": "/new-portfolio/static/media/icon-svg.2a146488.svg"
+  },
+  {
+    "revision": "b01e5f6f03f92830e47aa2aa1753344e",
+    "url": "/new-portfolio/static/media/interactive.b01e5f6f.svg"
+  },
+  {
+    "revision": "23c006a526eedc9cc4551e53bb0d1fed",
+    "url": "/new-portfolio/static/media/logo-tw.23c006a5.svg"
+  },
+  {
+    "revision": "d3a09518b82e44fe541b1a5146a9839b",
+    "url": "/new-portfolio/static/media/mobile.d3a09518.svg"
+  },
+  {
+    "revision": "4a8867748a6a82c3cf6225b5ca935965",
+    "url": "/new-portfolio/static/media/responsive.4a886774.svg"
+  },
+  {
+    "revision": "3156d6a10f652fe0b4922f6a62a4ddf8",
+    "url": "/new-portfolio/static/media/team-player.3156d6a1.svg"
+  }
+]);
